2b62c1bd427a5d77e37d712ad5ac96c1be97ca61
robbykansas <lomiver@gmail.com>
2025-06-11 22:55:16 +0700

    add cards

--- src/recommendations.tsx
+++ src/recommendations.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
-// import { Search } from 'lucide-react';
 import './Recommendations.css';
+import mal from './assets/mal-logo.png'
+import anilist from './assets/anilist-logo.png'
 
 interface RecommendationResponse {
-  recommendations?: any[];
+  recommendations?: [
+    {
+      title: string;
+      description: string;
+      genres: string[];
+      id: number;
+      idMal: number;
+      format: string;
+      averageScore: number;
+      popularity: number;
+      cover: string
+    }
+  ];
   error?: string;
 }
 
@@ -28,7 +41,7 @@ const RecommendationUI: React.FC = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ text_input: query }),
+        body: JSON.stringify({ input_text: query }),
       });
 
       if (!response.ok) {
@@ -36,6 +49,7 @@ const RecommendationUI: React.FC = () => {
       }
 
       const data = await response.json();
+
       setResults(data);
     } catch (err) {
       setError(err instanceof Error ? err.message : 'An error occurred');
@@ -52,6 +66,12 @@ const RecommendationUI: React.FC = () => {
 
   return (
     <div className={`recommendations-container ${hasSearched ? 'top' : 'centered'}`}>
+      { !hasSearched && (
+        <div className="app-container">
+          <h1 className="app-title">WeeBot</h1>
+          <p className="app-tagline">Recommend anime based on input with vector DB</p>
+        </div>
+      )}
       <input 
         type="text"
         value={query}
@@ -61,12 +81,37 @@ const RecommendationUI: React.FC = () => {
         className='recommendations-text'
         disabled={loading}
       />
+      { loading && (
+        <span className='loader'></span>
+      )}
 
       { hasSearched && (
-      <div>
-        <p>`${results?.recommendations}`</p>
+      <div className='result-container'>
+        {results?.recommendations?.map(item => (
+          <div className='cards-container'>
+            <div className='cards-description-container'>
+              <h2 className='desc-title'>{item.title}</h2>
+              <p className='desc-description' dangerouslySetInnerHTML={{ __html: item.description }}></p>
+              <div className='desc-info'>
+                <span className='info-tag'>{item.format}</span>
+                <span className='info-tag'>Score: {item.averageScore}</span>
+                <span className='info-tag'>Popularity: {item.popularity}</span>
+                <a href={`https://anilist.co/anime/${item.idMal}`} target="_blank" rel="noopener noreferrer" className='info-anilist'><img src={anilist} alt='anilist'/></a>
+                <a href={`https://myanimelist.net/anime/${item.idMal}`} target="_blank" rel="noopener noreferrer" className='info-mal'><img src={mal} alt='mal'/></a>
+              </div>
+              <div className='desc-genres'>
+                {item?.genres?.map(genre => (
+                  <span className='genre-tag'>{genre}</span>
+                ))}
+              </div>
+            </div>
+            <div className='cards-image'>
+              <img src={item.cover} alt='cover'/>
+            </div>
+          </div>
+        ))}
       </div>
-      )};
+      )}
     </div>
   )
 }
